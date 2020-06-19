b3a7458f048b2b27900e29172cbd0133c51017b7
wakwak_koba <5591750+wakwak-koba@users.noreply.github.com>
2020-06-19 11:28:21 +0900

    add example

---
+++ examples/NimBLE_CatchNotifies/NimBLE_CatchNotifies.ino
@@ -0,0 +1,48 @@
+/*
+ * https://twitter.com/wakwak_koba/
+ */
+
+#include <NimBLEDevice.h>
+
+static void notifyCallback(BLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify)  {
+  Serial.printf("notifyCallback: %s %s handle: %d value:", pRemoteCharacteristic->getRemoteService()->getClient()->getPeerAddress().toString().c_str(), pRemoteCharacteristic->getUUID().toString().c_str(), pRemoteCharacteristic->getHandle());
+  for (int i=0; i<length; i++)
+    Serial.printf(" %02x", pData[i]);
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  NimBLEDevice::init("");
+  auto* pBLEScan = NimBLEDevice::getScan();
+  pBLEScan->setActiveScan(true);
+
+  Serial.println("wait 10 secs..");
+  auto pScanResults = pBLEScan->start(10);
+
+  for (int i = 0; i < pScanResults.getCount(); i++) {
+    auto advertisedDevice = pScanResults.getDevice(i);
+    if (advertisedDevice.haveServiceUUID())  {
+      Serial.print("Found Device ");
+      Serial.print(advertisedDevice.toString().c_str());
+      auto pClient = NimBLEDevice::createClient();
+      if(pClient && pClient->connect(&advertisedDevice)) {
+        auto pServices = pClient->getServices();
+        for (auto pService : *pServices)  {
+          auto pCharacteristics = pService->getCharacteristics();
+          for (auto pCharacteristic : *pCharacteristics)
+            if(pCharacteristic->canNotify())
+              if(pCharacteristic->registerForNotify(notifyCallback)) {
+                Serial.printf("registerForNotify: %s %s handle:%d", pClient->getPeerAddress().toString().c_str(), pCharacteristic->getUUID().toString().c_str(), pCharacteristic->getHandle());
+                Serial.println();
+              }
+        }
+      }
+    }
+  }
+}
+
+void loop() {
+  delay(1);
+}
