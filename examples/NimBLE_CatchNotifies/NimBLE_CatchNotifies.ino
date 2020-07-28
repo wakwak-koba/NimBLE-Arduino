bc8d99aa9445d5d13d6ed9d2e77219df1fe5aebd
wakwak_koba <5591750+wakwak-koba@users.noreply.github.com>
2020-07-28 17:31:58 +0900

    fix example

--- examples/NimBLE_CatchNotifies/NimBLE_CatchNotifies.ino
+++ examples/NimBLE_CatchNotifies/NimBLE_CatchNotifies.ino
@@ -11,6 +11,13 @@ static void notifyCallback(BLERemoteCharacteristic* pRemoteCharacteristic, uint8
   Serial.println();
 }
 
+static void indicateCallback(BLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify)  {
+  Serial.printf("indicateCallback: %s %s handle: %d value:", pRemoteCharacteristic->getRemoteService()->getClient()->getPeerAddress().toString().c_str(), pRemoteCharacteristic->getUUID().toString().c_str(), pRemoteCharacteristic->getHandle());
+  for (int i=0; i<length; i++)
+    Serial.printf(" %02x", pData[i]);
+  Serial.println();
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -31,17 +38,24 @@ void setup() {
         auto pServices = pClient->getServices(true);
         for (auto pService : *pServices)  {
           auto pCharacteristics = pService->getCharacteristics(true);
-          for (auto pCharacteristic : *pCharacteristics)
-            if(pCharacteristic->canNotify())
-              if(pCharacteristic->registerForNotify(notifyCallback)) {
-                Serial.printf("registerForNotify: %s %s handle:%d", pClient->getPeerAddress().toString().c_str(), pCharacteristic->getUUID().toString().c_str(), pCharacteristic->getHandle());
-                Serial.println();
-              }
+          for (auto pCharacteristic : *pCharacteristics) {
+            if(pCharacteristic->canNotify() && pCharacteristic->subscribe(true, notifyCallback)) {
+              Serial.printf("registerForNotify: %s %s handle:%d", pClient->getPeerAddress().toString().c_str(), pCharacteristic->getUUID().toString().c_str(), pCharacteristic->getHandle());
+              Serial.println();
+            }
+            if(pCharacteristic->canIndicate() && pCharacteristic->subscribe(false, indicateCallback)) {
+              Serial.printf("registerForIndicate: %s %s handle:%d", pClient->getPeerAddress().toString().c_str(), pCharacteristic->getUUID().toString().c_str(), pCharacteristic->getHandle());
+              Serial.println();
+            }
+          }    
         }
       }
       Serial.println();
     }
   }
+  
+  if(NimBLEDevice::getClientList()->size() == 0)
+    ESP.restart();
 }
 
 void loop() {
